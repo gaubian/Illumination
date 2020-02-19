92a35158707876ee956c7db64e8f763bc40d2ea5
Delphine Salvy <delphine.salvy@icloud.com>
2020-02-19 17:32:23 +0100

    Code 2^k bugué

--- zephyr.py
+++ zephyr.py
@@ -1,9 +1,11 @@
 from pipou import parse
+import random
 
 (tab, n, m) = parse()
 # tab is an array of array st tab[i] is the list of lights for switch i
 # n is the number of lights
 # m is the number of switches
+GRAND_NOMBRE = 1000
 
 output = open("output_greedy.txt", "r")
 output = output.read()
@@ -20,4 +22,53 @@ def nombre_lampes_allumees(tab, n, output):
     return sum(lampes)
 
 
-print(nombre_lampes_allumees(tab, n, output))
+#print(nombre_lampes_allumees(tab, n, output))
+def list_output(interrupteurs):
+    return [i for i in range(m) if interrupteurs[i]]
+
+def list_interrupteurs(output):
+    interrupteurs = [False for _ in range(m)]
+
+    for switch in output:
+        interrupteurs[switch] = True
+
+    return interrupteurs
+
+
+def amelioration(liste_a_changer, m, n, output):    
+    interrupteurs = list_interrupteurs(output)
+
+    def aux(liste_a_changer, maxi, interrupteurs):
+        if liste_a_changer == []:
+            return maxi, interrupteurs
+
+        else:
+            interrupteurs[liste_a_changer[0]] = not interrupteurs[liste_a_changer[0]]
+            maxi = max(maxi, nombre_lampes_allumees(tab, n, list_output(interrupteurs)))
+            maxiN, interrupteursN = aux(liste_a_changer[1:], maxi, interrupteurs)
+
+            if maxiN > maxi:
+                maxi = maxiN
+                interrupteurs = interrupteursN
+
+            else:
+                interrupteurs[liste_a_changer[0]] = not interrupteurs[liste_a_changer[0]]
+
+            return maxi, interrupteurs
+
+    return aux(liste_a_changer, m, interrupteurs)
+
+def k_amelioration(k, m, n, output):
+    nombre_lampes = nombre_lampes_allumees(tab, n, output)
+
+    for i in range(GRAND_NOMBRE):
+        liste_a_changer = random.sample(list(range(m)), k)
+
+        maxi, interrupteurs = amelioration(liste_a_changer, m, n, output)
+        if maxi > nombre_lampes:
+            nombre_lampes = maxi
+            output = list_output(interrupteurs)
+
+    return output
+
+print(k_amelioration(10, m, n, output))
