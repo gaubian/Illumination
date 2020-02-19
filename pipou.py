c061c7201f4e3b82e365369db175b3c80f5d7fd8
aubian <aubian@zamokv5.crans.org>
2020-02-19 16:28:35 +0100

    First greedy solution

--- pipou.py
+++ pipou.py
@@ -10,6 +10,25 @@ def parse():
     return (tab, n, m)
 
 def greedy():
-    inp = parse()
+    (G, n, m) = parse()
+    lights = n * [False]
+    switches = m * [False]
     while True:
+        b = False
+        for i in range(m):
+            nb = 0
+            for j in G[i]:
+                if not lights[j]:
+                    nb += 1
+            if 2 * nb > len(G[i]):
+                b = True
+                switches[i] = not switches[i]
+                for j in G[i]:
+                    lights[j] = not lights[j]
+        if not b:
+            break
+    return [i for i in range(m) if switches[i]]
 
+def output_sol(tab):
+    for x in tab:
+        print(x)
