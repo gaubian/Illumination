f4bcf621c9a14ad232ca0bbc023b8ef995b0fd83
Delphine Salvy <delphine.salvy@icloud.com>
2020-02-19 16:25:26 +0100

    Fonction nombre lampes allumées

--- zephyr.py
+++ zephyr.py
@@ -1,1 +1,20 @@
-print("Je suis le plus beau")
+from pipou import parse
+
+(tab, n, m) = parse()
+# tab is an array of array st tab[i] is the list of lights for switch i
+# n is the number of lights
+# m is the number of switches
+
+output = open("output.txt", "r")
+output = output.read()
+
+l = list(map(int, ouput.split(',')))
+
+def nombre_lampes_allumees(tab, m, output):
+    lampes = [0 for _ in range(m)]
+
+    for switch in output:
+        for lamp in tab[switch]:
+            lampes[lamp] = 1 - lampes[lamp]
+
+    return sum(lampes)
