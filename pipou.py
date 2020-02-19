c8b8e34a94d9dcf0672bcb70e27a91c1295ab486
aubian <aubian@zamokv5.crans.org>
2020-02-19 16:12:55 +0100

    Added the parser

--- pipou.py
+++ pipou.py
@@ -1,1 +1,7 @@
-print("PIPOU")
+def parse():
+    tab = []
+    [n, m] = list(map(int, input().split(',')))
+    for i in range(m):
+        l = list(map(int, input().split(',')))
+        tab.append(l)
+    return tab
